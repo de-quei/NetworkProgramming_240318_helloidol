c74c990fe5d804afe42423a966e84acedc907245
hyunji Kim <s2208@e-mirim.hs.kr>
2024-03-25 12:16:47 +0900

    views -> templates에 context 전달

--- playground/views.py
+++ playground/views.py
@@ -5,7 +5,12 @@ def say_hello(request):
     return HttpResponse("Hello World!")
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html',
+                  {'name': '지우야', 'greeting':'안녀엉~'})
 
 def say_bye_html(request):
-    return render(request, 'playground/bye.html')
+    context = {
+        'singer': '백지영',
+        'title': '총 맞은 것처럼'
+    }
+    return render(request, 'playground/bye.html', context=context)
